2bdd7192e6a0fc1b6dd0c50569bd1e3b3b2ed86a
atharvsalvi <atharv.salvi22@gmail.com>
2026-09-22 16:09:12 +0530

    initial

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello from FastAPI"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
